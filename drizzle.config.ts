9bb5497b46ab43c330b7f9511ef8b961d27ff4b5
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-10 19:49:41 +0300

    drizzle studio and db just connected

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,15 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from "drizzle-kit"
 
-import { env } from "~/env";
+import { env } from "~/env"
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["igor_boiko_the_dev_v2_*"],
-} satisfies Config;
+	schema: "./src/server/db/schema.ts",
+	driver: "pg",
+	dbCredentials: {
+		connectionString: env.POSTGRES_URL,
+	},
+	url: env.POSTGRES_URL,
+	tablesFilter: ["igor_boiko_the_dev_v2_*"],
+	dialect: "postgresql",
+} as Config
+// } satisfies Config // lol dont working hahahahah
