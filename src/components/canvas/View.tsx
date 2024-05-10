451a02747438aecfe4de52418b696e87e74a4b7e
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-10 19:01:35 +0300

    any fix

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -7,41 +7,43 @@ import { Three } from "@/helpers/components/Three"
 type ViewProps = {
 	children: React.ReactNode
 	orbit?: boolean
-	[key: string]: any
+	props: {
+		props: [key: string]
+	}
 }
 
-// const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
-// 	const localRef = useRef<HTMLDivElement>(null)
-// 	useImperativeHandle(ref, () => localRef.current)
-
-// 	return (
-// 		<>
-// 			<div ref={localRef} {...props} />
-// 			<Three>
-// 				<ViewImpl track={localRef as React.MutableRefObject<HTMLDivElement>}>
-// 					{children}
-// 					{orbit && <OrbitControls />}
-// 				</ViewImpl>
-// 			</Three>
-// 		</>
-// 	)
-// })
-const View = forwardRef((props: ViewProps, ref) => {
+const View = forwardRef(({ children, orbit, ...props }: ViewProps, ref) => {
 	const localRef = useRef<HTMLDivElement>(null)
 	useImperativeHandle(ref, () => localRef.current)
 
 	return (
 		<>
-			<div ref={localRef} {...props.props} />
+			<div ref={localRef} {...props} />
 			<Three>
 				<ViewImpl track={localRef as React.MutableRefObject<HTMLDivElement>}>
-					{props.children}
-					{props.orbit && <OrbitControls />}
+					{children}
+					{orbit && <OrbitControls />}
 				</ViewImpl>
 			</Three>
 		</>
 	)
 })
+// const View = forwardRef((props: ViewProps, ref) => {
+// 	const localRef = useRef<HTMLDivElement>(null)
+// 	useImperativeHandle(ref, () => localRef.current)
+
+// 	return (
+// 		<>
+// 			<div ref={localRef} {...props.props} />
+// 			<Three>
+// 				<ViewImpl track={localRef as React.MutableRefObject<HTMLDivElement>}>
+// 					{props.children}
+// 					{props.orbit && <OrbitControls />}
+// 				</ViewImpl>
+// 			</Three>
+// 		</>
+// 	)
+// })
 View.displayName = "View"
 
 export default View
