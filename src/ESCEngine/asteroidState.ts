775f11bf52658d9572fa682df98b2b784df05cc7
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-18 18:14:00 +0300

    smth

--- src/ESCEngine/asteroidState.ts
+++ src/ESCEngine/asteroidState.ts
@@ -1,3 +1,4 @@
+import { pipe } from "fp-ts/lib/function"
 import { World } from "miniplex"
 import createReactAPI from "miniplex-react"
 import { THREE } from "~/exp"
@@ -12,35 +13,75 @@ export type Asteroid = {
 /* Create a Miniplex world that holds our entities */
 export const asteroidWorld = new World<Asteroid>()
 
-const _genRange = 5
-const _genPose = () => (Math.random() - 0.5) * _genRange
+const _genPose = () => Math.random() - 0.5
+const _getVector = (range: number) =>
+	new THREE.Vector3(_genPose() * range, _genPose() * range, _genPose() * range)
 const createSwarm = (ammouns: number = 123) => {
 	for (let i = 0; i < ammouns; i++) {
 		const Asteroid = asteroidWorld.add({
-			position: new THREE.Vector3(_genPose(), _genPose(), _genPose()),
-			velocity: new THREE.Vector3(0, 0, 0),
+			position: _getVector(5),
+			velocity: _getVector(0.04),
 			rotation: new THREE.Euler(),
 		})
 	}
 }
-createSwarm(50)
+createSwarm(123)
 
-/* Create a bunch of systems: */
-const velocitySystem = () => {
-	const velocityFading = 0.99
-	for (const a of asteroidWorld.entities) {
-		a.position.add(a.velocity)
-		a.velocity.multiplyScalar(velocityFading)
+// smth
+
+const velocitySmoothCap = (max: number) => (v: THREE.Vector3) => {
+	const targetVelocity = v.clone()
+	const currentSpeed = v.length()
+
+	if (currentSpeed > max) {
+		const speedDifference = currentSpeed - max
+		const capFactor = speedDifference / currentSpeed
+
+		targetVelocity.multiplyScalar(1 - capFactor)
+		targetVelocity.add(v.multiplyScalar(capFactor))
 	}
-}
-const pointGravitySystem = () => {
-	const gravitationPoint = new THREE.Vector3(0, 0, 0)
-	const minGravityMagnitude = 0.0001
-	const maxGravityMagnitude = 0.009
-	for (const a of asteroidWorld.entities) {
-		const direction = gravitationPoint.clone().sub(a.position).normalize()
 
-		const distance = gravitationPoint.distanceTo(a.position)
+	return targetVelocity
+}
+
+// SYSTEMS
+const lambdaSystem =
+	<TEntity>(fn: (a: TEntity) => void) =>
+	(entities: TEntity[]) => {
+		for (const entity of entities) {
+			fn(entity)
+		}
+		return entities
+	}
+const velocitySystem = (entities: Asteroid[]) => {
+	var velocityFading = 0.99
+	for (const entity of entities) {
+		entity.position.add(entity.velocity)
+		entity.velocity.multiplyScalar(velocityFading)
+	}
+	return entities
+}
+const distanceClampSystem = (entities: Asteroid[]) => {
+	var centerPoint = new THREE.Vector3(0, 0, 0)
+	var radius = 2
+	for (const entity of entities) {
+		const distance = centerPoint.distanceTo(entity.position)
+		if (distance > radius) {
+			// const direction = centerPoint.clone().sub(entity.position).normalize().multiplyScalar(radius)
+			entity.velocity.set(0, 0, 0)
+			// entity.position.copy(direction.multiplyScalar(-1))
+		}
+	}
+	return entities
+}
+const pointGravitySystem = (entities: Asteroid[]) => {
+	var gravitationPoint = new THREE.Vector3(0, 0, 0)
+	var minGravityMagnitude = 0.0001
+	var maxGravityMagnitude = 0.009
+	for (const entity of entities) {
+		const direction = gravitationPoint.clone().sub(entity.position).normalize()
+
+		const distance = gravitationPoint.distanceTo(entity.position)
 		const gravityMagnitude = 0.005 / distance
 
 		const gravityMagnitude2 = Math.max(
@@ -48,15 +89,45 @@ const pointGravitySystem = () => {
 			Math.min(maxGravityMagnitude, gravityMagnitude),
 		)
 
-		a.velocity.add(direction.multiplyScalar(gravityMagnitude2))
+		entity.velocity.add(direction.multiplyScalar(gravityMagnitude2))
 	}
+	return entities
+}
+const keepDistanceFromEachOtherSystem = (entities: Asteroid[]) => {
+	var minDistance = 0.1
+	for (const entity of entities) {
+		for (const naighbour of entities) {
+			if (entity === naighbour) continue
+
+			const directionAB = naighbour.position.clone().sub(entity.position)
+
+			directionAB.normalize()
+
+			const currentDistance = entity.position.distanceTo(naighbour.position)
+
+			if (currentDistance < minDistance) {
+				const adjustmentAmount = (minDistance - currentDistance) / 2
+				const adjustmentVector = directionAB.multiplyScalar(adjustmentAmount)
+
+				entity.velocity.add(adjustmentVector)
+				naighbour.velocity.sub(adjustmentVector)
+			}
+		}
+	}
+	return entities
 }
 
 // every frame systems update loop
 let frameCount = 0
 const systemUpdateLoop = () => {
-	velocitySystem()
-	pointGravitySystem()
+	pipe(
+		asteroidWorld.entities,
+		pointGravitySystem,
+		keepDistanceFromEachOtherSystem,
+		distanceClampSystem,
+		velocitySystem,
+		lambdaSystem((a) => velocitySmoothCap(0.5)(a.velocity)),
+	)
 
 	frameCount++
 	if (frameCount % 100 === 0) {
