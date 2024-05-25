defb34af0b37ccb1270f0ef6d2705626cdc3537d
Ygor Boiko Mikhailovich <113383645+Rrailnechex@users.noreply.github.com>
2024-05-25 22:51:25 +0300

    just deleted for now. later will use just canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,65 +3,27 @@ import { TestVizDataList } from "../components/myFirstFrickingReactComponentsOMG
 import TestThreeSceneCanvas from "../components/_test/TestThreeScene"
 import { dynamicAsteroidView } from "~/ESCEngine/dynamic"
 
-const AsteroidView = dynamicAsteroidView()
+const newSection = (sectionName?: string) => (
+	<div id="section" className="h-[500px] w-full flexflex-col">
+		<div className="h-[80%] w-full flex flex-row bg-gray-500">
+			<p className="text-2xl font-bold text-white">{sectionName}</p>
+		</div>
+	</div>
+)
 
+/**
+ *  new one
+ */
 export default async function HomePage() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center   text-white">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				{/* About me section */}
-				<div id="AboutMeSection" className="flex flex-col  h-96 w-full bg-violet-500">
-					<div className="h-full w-full grid grid-cols-2 ">
-						<div className="h-full w-full relative">
-							<p>Igor Boiko The Dev V2</p>
-							<p className="text-2xl font-bold">Igor Boiko The Dev V3</p>
-							<p className="text-2xl font-bold">Igor Boiko The Dev V4</p>
-							<p className="text-2xl font-bold">Igor Boiko The Dev V4</p>
-							<TestThreeSceneCanvas />
-						</div>
-						<div>
-							<AsteroidView />
-						</div>
-					</div>
-				</div>
-				{/* About me section */}
-
-				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					Create <span className="text-[hsl(280,100%,70%)]">T2</span> App
-				</h1>
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/usage/first-steps"
-						target="_blank">
-						<h3 className="text-2xl font-bold">First Steps →</h3>
-						<div className="text-lg">
-							Just the basics - Everything you need to know to set up your database and
-							authentication.
-						</div>
-					</Link>
-
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="https://create.t3.gg/en/introduction"
-						target="_blank">
-						<h3 className="text-2xl font-bold">Documentation →</h3>
-						<div className="text-lg">
-							Learn more about Create T3 App, the libraries it uses, and how to deploy it.
-						</div>
-					</Link>
-				</div>
-
-				<div className="w-full h-fit flex flex-col gap-4">
-					<div className="w-full h-fit bg-slate-900">
-						<TestThreeSceneCanvas />
-					</div>
-					<div className="w-full h-fit bg-slate-900">
-						<AsteroidView />
-					</div>
-					<TestVizDataList />
-				</div>
-			</div>
+		<main className="flex min-h-screen flex-col items-center justify-center   ">
+			{newSection("LandSection - DESIGNER/DEVELOPER")}
+			{newSection("AboutSection")}
+			{newSection("AboutSection")}
+			{newSection("AboutSection")}
+			{newSection("AboutSection")}
+			{newSection("AboutSection")}
+			{newSection("AboutSection")}
 		</main>
 	)
 }
